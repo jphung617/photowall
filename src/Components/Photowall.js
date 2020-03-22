3fafe2c55d4d5a5f086a50c1c0176016596fb0da
Jason Phung <jasonphung@Jasons-MacBook-Pro.local>
2020-03-22 18:53:53 -0400

    deploy photowall to github pages

---
+++ src/Components/Photowall.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Photo from './Photo'
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+
+//anchor tag, href attribute
+
+function Photowall(props) {
+    return  <div>
+                 <Link to="/AddPhoto" className='add-icon'>  </Link>
+                 <div className='photo-grid'>
+                     { props.posts
+                     .sort(function(x,y) {
+                         return y.id - x.id
+                     })
+                     .map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>) }
+                 </div>
+            </div>
+}
+
+Photowall.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
+
+export default Photowall
---
+++ src/Components/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Title(props) {
+    return  <h1> {props.subject} </h1>
+}
+
+export default Title
